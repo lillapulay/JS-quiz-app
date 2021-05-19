96a658a33819745132a64274742f83ed111b7daa
lillapulay <59831508+lillapulay@users.noreply.github.com>
2021-05-19 12:09:38 +0200

    HighScores sorted, saved in localStorage, limited to 5

--- js/end.js
+++ js/end.js
@@ -2,15 +2,32 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || []; // If localStorage is empty, initializes an array
+const MAX_HIGH_SCORES = 5;
+
 finalScore.innerText = mostRecentScore;
 
 
 username.addEventListener('keyup', () => {
-    console.log(username.value);
     saveScoreBtn.disabled = !username.value
 })
 
 saveHighScore = (e) => {
-    console.log("clicked");
     e.preventDefault();
+
+    const score = {
+        score: Math.floor(Math.random() *100), // Testing if scores are sorted properly
+        name: username.value
+    };
+    highScores.push(score);
+
+    highScores.sort((a,b) => b.score - a.score) /* Sorts the array - biggest to lowest */   
+    
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/');
+    
+    console.log(highScores);
 }
